4f8361f970cfb5dc0001a2ee9d2b09b52178dd93
Mister-New-Life <tsumbalyk255@gmail.com>
2019-10-21 21:32:47 +0300

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,27 @@ const 	gulp = require('gulp'),
 		del = require('del'),
 		rename = require('gulp-rename'),
 		concat = require('gulp-concat'),
-		concatCss = require('gulp-concat-css'),
 		csso = require('gulp-csso'),
 		autoprefixer = require('gulp-autoprefixer'),
 		imagemin = require('gulp-imagemin'),
 		uglify = require('gulp-uglify'),
 		imageminMozjpeg = require('imagemin-mozjpeg'),
-		browserSync = require('browser-sync').create();
+		browserSync = require('browser-sync').create();	
 	
-gulp.task('clean', function(done) {
+gulp.task('clean', async () => {
 	return del(['dist']);
-	done();
 });
 		
-gulp.task('styles', function (done) {
-	gulp.src('app/sass/*.sass')
-	.pipe(sass({
-		includePaths: require('node-bourbon').includePaths
-	}).on('error', sass.logError))
-	.pipe(rename({suffix: '.min', prefix : ''}))
+gulp.task('styles', () => {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(rename({suffix: '.min', prefix: ''}))
 	.pipe(autoprefixer({
 		browsers: ['last 15 versions'],
 		cascade: false
 	}))
 	.pipe(csso(''))
 	.pipe(gulp.dest('dist/css'));
-	done();
 });
 
 gulp.task('compress', () =>
@@ -40,59 +35,46 @@ gulp.task('compress', () =>
 	.pipe(gulp.dest('dist/img/'))
 );
 
-gulp.task('copyJquery', function (done) {
-	return gulp.src('app/libs/jquery/jquery.min.js')
-	.pipe(gulp.dest('dist/js'));
-	done();
-});
-
-gulp.task('copyfontAwesome', function(done) {
-	gulp.src('app/libs/font-awesome/*.{ttf,woff,woff2,eot,svg}')
-	.pipe(gulp.dest('dist/css/font-awesome/webfonts/'));
-	done();
-});
-
-gulp.task('scriptsConcat', function() {
+gulp.task('scriptsConcat', () => {
 	return gulp.src('app/libs/**/*.js', '!app/libs/jquery/jquery.min.js')
 	.pipe(concat('plugin.min.js'))
 	.pipe(uglify(''))
 	.pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('scriptsCommon', function() {
+gulp.task('scriptsCommon', () => {
 	return gulp.src('app/js/*.js')
-	.pipe(uglify(''))
 	.pipe(concat('common.min.js'))
+	.pipe(uglify(''))
 	.pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('vendorCss', function (done) {
+gulp.task('vendorCss', () => {
 	return gulp.src('app/libs/**/*.css')
-	.pipe(concatCss("vendor.css"))   
 	.pipe(csso('')) 
 	.pipe(rename("vendor.min.css"))
 	.pipe(gulp.dest('dist/css'));
-	done();
 }); 
 
-gulp.task('templates', function(done) {
+gulp.task('templates', () => {
 	var YOUR_LOCALS = {};
-	gulp.src('app/**/*.jade')
+	return gulp.src('app/**/*.jade')
 		.pipe(jade({
 		locals: YOUR_LOCALS,
 		pretty: true
 	}))
 	.pipe(gulp.dest('./dist/'));
-	done();
 });
 
-gulp.task('fontsdist', function(done) {
-	return gulp.src('app/fonts/*/**')   
-	.pipe(gulp.dest('dist/fonts'));
-	done();
-});
+gulp.task('copyFonts', async () => {
+	let fontAwesome = gulp.src('app/libs/font-awesome/*.{ttf,woff,woff2,eot,svg}')
+		.pipe(gulp.dest('dist/css/font-awesome/webfonts/'));	
 
-gulp.task('browser-sync', function() {
+	let fonts = gulp.src('app/fonts/*/**')   
+		.pipe(gulp.dest('dist/fonts'));
+})
+
+gulp.task('browser-sync', () => {
 	browserSync.init({
 		server: {
 			baseDir: "./dist"
@@ -102,22 +84,12 @@ gulp.task('browser-sync', function() {
 	});
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
 	gulp.watch('app/sass/*.sass', gulp.series('styles'));
 	gulp.watch('app/libs/**/*.js', gulp.series('scriptsConcat'));
 	gulp.watch('app/js/*.js', gulp.series('scriptsCommon'));	
 	gulp.watch('app/**/*.jade', gulp.series('templates'));
 });
 
-gulp.task('default', gulp.series( gulp.series(	
-									'clean',
-									'styles',							
-									'scriptsConcat',
-									'scriptsCommon',
-									'copyJquery',
-									'copyfontAwesome',
-									'vendorCss',							
-									'templates',
-									'compress',
-									'fontsdist'), 
-		gulp.parallel('watch', 'browser-sync')));
+gulp.task('default', gulp.series( gulp.series('clean','templates','styles','vendorCss', 'scriptsConcat', 
+			'scriptsCommon','copyFonts','compress'), gulp.parallel('watch', 'browser-sync')));
